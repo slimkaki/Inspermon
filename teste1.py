8501eef22295e0e56c6fc36381a986fe88bb6a24
HeMatita <matheussoares.augusto@hotmail.com>
2017-04-07 10:20:09 -0300

    Add files via upload

--- teste1.py
+++ teste1.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 """
 Spyder Editor
 
@@ -12,14 +12,22 @@ def print_slow(str):
         sys.stdout.write(letter)
         sys.stdout.flush()
         time.sleep(0.05)
+def print_sleep(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.1)
 #Início
-print_slow("Bem-vindo ao Inspermon!")
-print_slow("Deseja iniciar o jogo? 0 para sair, 1 para iniciar e 2 para continuar seu jogo salvo: ")
+print_slow("Inspermon!"+"\n")
+print_slow("\n"+"Oque deseja fazer?"+"\n"+"Pressione"+"\n"+" 0 para sair"+"\n"+" 1 para iniciar um jogo novo"+"\n"+" 2 para continuar seu jogo salvo")
 escolha=int(input("\n"+"->"))
 if (escolha==1):
     print_slow("Boa sorte")
+elif (escolha==2):
+    print_slow("Aguarde uns instantes. . . . . . . . . . . . ")
 else:
     print_slow("Até a próxima vez então")
+    
 
 #perguntas de inicio
 print_slow("\n"+"Bem-vindo ao Inspermon! Qual o seu nome? ")
@@ -32,11 +40,11 @@ secure_random=random.SystemRandom()
 if (semestre==1):
 	lista_inspermon_inicial={"Pelicano": "Fogo","Carareto": "Elétrico","Luciano": "Água"}
 
-print_slow("Para começar, nessa sua incrível aventura, você terá de escolher um Inspermon inicial para te acompanhar! "+"\n")
+print_slow("Para iniciar, essa sua incrível jornada, escolha um Inspermon inicial para te acompanhar! "+"\n")
 for f, g in lista_inspermon_inicial.items():
 	print("\n"+"{0}: {1}".format(f, g))
 inspermon_inicial=input("\n"+"Escolha apenas um inspermon! ")
-print_slow("Que otima escolha"+"\n"+"Agora vamos iniciar a aventura de verdade"+"\n")
+print_slow("Que ótima escolha"+"\n"+"Agora vamos iniciar a aventura de verdade"+"\n")
 print_slow("{0}, você acorda e está em sua casa em Olímpia Town".format(nome))
 
 
@@ -47,9 +55,11 @@ os.startfile('tema.mp3')
 print_slow("\n"+"Você deseja passear ou dormir?")
 escolha=input("\n"+"->").title()
 while escolha=="Dormir":
-	print_slow("..........."+"\n"+"Você se sente cheio de determinação.")
-	print_slow("\n"+"Agora você deseja passear ou dormir?")
-	escolha=input("\n"+"->").title()
+    print_sleep(". . . . . . . . . . . . . . . . ."+"\n")
+    print_slow("Você se sente cheio de determinação.")
+    print_slow("\n"+"Agora você deseja passear ou dormir?")
+    escolha=input("\n"+"->").title()
+
 
 
 #Opções de lugares para ir
